54e8bf17af9a820b4c8f8a203d16fbae036d0d3c
Yakovlev Oleg Alekseevich <yakovlev.oleg2010@gmail.com>
2015-02-23 23:17:29 +0300

    123

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -4,14 +4,13 @@ import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import model.ConjugationDesc;
 import model.ConjugationManager;
 import model.InnerConjugationValidator;
 
@@ -66,35 +65,28 @@ public class MainViewController {
     private Line connector_ln;
 
     @FXML
-    private Arc conjugation_cir;
+    private Circle conjugation_cir;
+
+    @FXML
+    private Circle conjugation_cir1;
 
     @FXML
     private Slider con_rad;
 
     @FXML
     void initialize() {
-        assert cir_1_y != null : "fx:id=\"cir_1_y\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert cir_2_x != null : "fx:id=\"cir_2_x\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert cir_2_y != null : "fx:id=\"cir_2_y\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert circle_1 != null : "fx:id=\"circle_1\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert circle_2 != null : "fx:id=\"circle_2\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert status_lbl != null : "fx:id=\"status_lbl\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert action_btn != null : "fx:id=\"action_btn\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert viewPane != null : "fx:id=\"viewPane\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert cir_1_r != null : "fx:id=\"cir_1_r\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert cir_2_r != null : "fx:id=\"cir_2_r\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert clear_btn != null : "fx:id=\"clear_btn\" was not injected: check your FXML file 'MainView.fxml'.";
-        assert cir_1_x != null : "fx:id=\"cir_1_x\" was not injected: check your FXML file 'MainView.fxml'.";
 
         // Configure bindings & listeners
 
         ChangeListener validationListener = (obs_v, old_v, new_v) -> {
-            if (InnerConjugationValidator.validate(circle_1, circle_2, conjugation_cir.getRadiusX())) {
+            if (InnerConjugationValidator.validate(circle_1, circle_2, conjugation_cir.getRadius())) {
                 conjugation_cir.setVisible(false);
+                //conjugation_cir1.setVisible(false);
                 status_lbl.setText("Possible");
                 action_btn.setDisable(false);
             } else {
                 conjugation_cir.setVisible(false);
+                //conjugation_cir1.setVisible(false);
                 status_lbl.setText("Impossible");
                 action_btn.setDisable(true);
             }
@@ -123,28 +115,27 @@ public class MainViewController {
         connector_ln.endYProperty().bindBidirectional(circle_2.centerYProperty());
 
         // Conjugation radius
-        conjugation_cir.radiusXProperty().bindBidirectional(con_rad.valueProperty());
-        conjugation_cir.radiusYProperty().bindBidirectional(con_rad.valueProperty());
+        conjugation_cir.radiusProperty().bindBidirectional(con_rad.valueProperty());
+        conjugation_cir1.radiusProperty().bindBidirectional(con_rad.valueProperty());
         con_rad.valueProperty().addListener((validationListener));
 
         // Event handling
 
         EventHandler<ActionEvent> action = (e) -> {
-            ConjugationDesc con_desc = ConjugationManager.calculate(circle_1, circle_2, conjugation_cir.getRadiusX());
-            conjugation_cir.setCenterX(con_desc.con_center.getX());
-            conjugation_cir.setCenterY(con_desc.con_center.getY());
-            conjugation_cir.setStartAngle(con_desc.start_angle);
-            if (con_desc.end_angle * con_desc.start_angle > 0) {
-                conjugation_cir.setLength(Math.abs(con_desc.end_angle - con_desc.start_angle));
-            } else if (con_desc.end_angle < 0) {
-                conjugation_cir.setLength(360.0 - (con_desc.start_angle + Math.abs(con_desc.end_angle)));
-            } else {
-                conjugation_cir.setLength(Math.abs(con_desc.start_angle) + con_desc.end_angle);
-            }
+            Point2D con_center = ConjugationManager.calculate(circle_1, circle_2, conjugation_cir.getRadius());
+            conjugation_cir.setCenterX(con_center.getX());
+            conjugation_cir.setCenterY(con_center.getY());
+            conjugation_cir.setVisible(true);
+            Point2D con_center1 = ConjugationManager.calculateInverse(circle_1, circle_2, conjugation_cir.getRadius());
+            conjugation_cir1.setCenterX(con_center1.getX());
+            conjugation_cir1.setCenterY(con_center1.getY());
             conjugation_cir.setVisible(true);
         };
 
-        EventHandler<ActionEvent> clear = (e) -> conjugation_cir.setVisible(false);
+        EventHandler<ActionEvent> clear = (e) -> {
+            conjugation_cir.setVisible(false);
+            conjugation_cir1.setVisible(false);
+        };
 
         action_btn.setOnAction(action);
         clear_btn.setOnAction(clear);
