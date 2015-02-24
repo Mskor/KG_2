6c192f3ffc787dbf77d48588c11f515de803518a
Yakovlev Oleg Alekseevich <yakovlev.oleg2010@gmail.com>
2015-02-24 10:03:24 +0300

    123

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,6 +17,8 @@ import model.InnerConjugationValidator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainViewController {
 
@@ -73,6 +76,8 @@ public class MainViewController {
     @FXML
     private Slider con_rad;
 
+    private Timer t;
+
     @FXML
     void initialize() {
 
@@ -94,19 +99,19 @@ public class MainViewController {
 
         // Red circle
         circle_1.centerXProperty().bindBidirectional(cir_1_x.valueProperty());
-        circle_1.centerXProperty().addListener(validationListener);
+        //circle_1.centerXProperty().addListener(validationListener);
         circle_1.centerYProperty().bindBidirectional(cir_1_y.valueProperty());
-        circle_1.centerYProperty().addListener(validationListener);
+        //circle_1.centerYProperty().addListener(validationListener);
         circle_1.radiusProperty().bindBidirectional(cir_1_r.valueProperty());
-        circle_1.radiusProperty().addListener(validationListener);
+        //circle_1.radiusProperty().addListener(validationListener);
 
         // Blue circle
         circle_2.centerXProperty().bindBidirectional(cir_2_x.valueProperty());
-        circle_2.centerXProperty().addListener(validationListener);
+        //circle_2.centerXProperty().addListener(validationListener);
         circle_2.centerYProperty().bindBidirectional(cir_2_y.valueProperty());
-        circle_2.centerYProperty().addListener(validationListener);
+        // circle_2.centerYProperty().addListener(validationListener);
         circle_2.radiusProperty().bindBidirectional(cir_2_r.valueProperty());
-        circle_2.radiusProperty().addListener(validationListener);
+        //circle_2.radiusProperty().addListener(validationListener);
 
         // Line connecting two centers
         connector_ln.startXProperty().bindBidirectional(circle_1.centerXProperty());
@@ -117,7 +122,7 @@ public class MainViewController {
         // Conjugation radius
         conjugation_cir.radiusProperty().bindBidirectional(con_rad.valueProperty());
         conjugation_cir1.radiusProperty().bindBidirectional(con_rad.valueProperty());
-        con_rad.valueProperty().addListener((validationListener));
+        //con_rad.valueProperty().addListener((validationListener));
 
         // Event handling
 
@@ -135,9 +140,54 @@ public class MainViewController {
         EventHandler<ActionEvent> clear = (e) -> {
             conjugation_cir.setVisible(false);
             conjugation_cir1.setVisible(false);
+            if (t != null) {
+                t.cancel();
+                t.purge();
+            }
         };
 
-        action_btn.setOnAction(action);
+        EventHandler<ActionEvent> animate = (e) -> {
+            Double max_con_rad = 100.0d;
+
+            TimerTask tt = new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(() -> {
+                        conjugation_cir.setRadius((System.currentTimeMillis() % 10000) / 100);
+                        if ((InnerConjugationValidator.validate(circle_1, circle_2, conjugation_cir.getRadius()))) {
+
+                            Point2D con_center = ConjugationManager.calculate(circle_1, circle_2, conjugation_cir.getRadius());
+                            conjugation_cir.setCenterX(con_center.getX());
+                            conjugation_cir.setCenterY(con_center.getY());
+                            conjugation_cir.setVisible(true);
+                            Point2D con_center1 = ConjugationManager.calculateInverse(circle_1, circle_2, conjugation_cir.getRadius());
+                            conjugation_cir1.setCenterX(con_center1.getX());
+                            conjugation_cir1.setCenterY(con_center1.getY());
+                            conjugation_cir1.setVisible(true);
+                        } else {
+                            conjugation_cir.setVisible(false);
+                            conjugation_cir1.setVisible(false);
+                        }
+                    });
+                }
+            };
+
+            t = new Timer();
+            t.schedule(tt, 0l, 10l);
+        };
+
+        action_btn.setOnAction(animate);
         clear_btn.setOnAction(clear);
+    }
+
+    private void setBindingsForConRad() {
+        // Conjugation radius
+        conjugation_cir.radiusProperty().bindBidirectional(con_rad.valueProperty());
+        conjugation_cir1.radiusProperty().bindBidirectional(con_rad.valueProperty());
+    }
+
+    private void remBindingsForConfRad() {
+        conjugation_cir.radiusProperty().unbindBidirectional(con_rad.valueProperty());
+        conjugation_cir1.radiusProperty().unbindBidirectional(con_rad.valueProperty());
     }
 }
